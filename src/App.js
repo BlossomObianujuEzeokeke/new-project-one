aa524261357342b939e2df51a0c95ab1f2194ccc
Blossom Obianuju Ezeokeke <ezeokekeobianuju@gmail.com>
2023-04-01 22:02:54 +0100

    useState

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import CounterClass from "./Component/CounterClass";
+import HookCounter from "./Component/HookCounter";
+import HookCounterTwo from "./Component/HookCounterTwo";
+import HookCounterThree from "./Component/HookCounterThree";
+
+class App extends Component {
+  render() {
+    return (
+      // <CounterClass />
+      // <HookCounter />
+      // <HookCounterTwo />
+      // <HookCounterThree />
+    );
+  }
+}
+
+export default App;
