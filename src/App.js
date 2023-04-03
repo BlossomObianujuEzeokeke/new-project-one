187fe36cb45def2308d13b0f3f27152172e098f0
Blossom Obianuju Ezeokeke <ezeokekeobianuju@gmail.com>
2023-04-03 18:40:04 +0100

    Data fetching

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import CounterClass from "./Component/CounterClass";
 import HookCounter from "./Component/HookCounter";
 import HookCounterTwo from "./Component/HookCounterTwo";
 import HookCounterThree from "./Component/HookCounterThree";
+import HookCounterFour from "./Component/HookCounterFour";
+import IntervalHookCounter from "./Component/IntervalHookCounter";
+import DataFetching from "./Component/DataFetching";
 
 class App extends Component {
   render() {
@@ -12,6 +15,11 @@ class App extends Component {
       // <HookCounter />
       // <HookCounterTwo />
       // <HookCounterThree />
+      // <HookCounterFour />
+      // <IntervalHookCounter />
+      <DataFetching />
+
+
     );
   }
 }
--- src/Component/HookCounter.js
+++ src/Component/HookCounter.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-function HookCounter() {
+// function HookCounter() {
 
-   const [count, setCount] = useState(0)
-    
+//    const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <button onClick = {() => setCount(count + 1)}> Count {count} </button>
+//     </div>
+//   )
+// }
+
+// export default HookCounter
+
+// ============================
+import React, { useState, useEffect } from "react";
+
+const HookCounter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  });
+
   return (
     <div>
-      <button onClick = {() => setCount(count + 1)}> Count {count} </button>
+      <button onClick={() => setCount(count + 1)}> Click {count} times</button>
     </div>
-  )
-}
+  );
+};
 
-export default HookCounter
+export default HookCounter;
